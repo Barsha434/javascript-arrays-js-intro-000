a9cfc84b9c969cea7f632a2118362e494bb0cb0d
Barsha Dahal <dahalbarsha19@gmail.com>
2020-06-02 23:06:29 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function accessElementInArray(array,index){
   return array[index];
 }
 
-function destructivelyRemovesElementFromBeginningOfArray(array,element){
+function destructivelyRemoveElementFromBeginningOfArray(array,element){
 array.shift(element);
 return array
 }
