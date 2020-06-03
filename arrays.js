693ac02b84c8551b091d1f12584cb0e43c318e56
Barsha Dahal <dahalbarsha19@gmail.com>
2020-06-03 00:23:25 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,3 @@
-
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array,element){
@@ -9,7 +8,6 @@ function destructivelyAddElementToBeginningOfArray(array,element){
 array.unshift(element);
 return array
 }
-
 function addElementToEndOfArray(array,element){
 array=[...array,element]
 return array
@@ -17,7 +15,7 @@ return array
 function destructivelyAddElementToEndOfArray(array,element){
   array.push(element);
   return array
-
+}
 function accessElementInArray(array,index){
   return array[index];
 }
